04f330f73e57edbb1d381fb36cce69178e726ad5
Conan Kunhwan Ahn <kunhwon@gmail.com>
2025-09-17 12:23:26 +0900

    예제 코드의 페이지 타이틀 추가

--- src/pages/page/index.tsx
+++ src/pages/page/index.tsx
@@ -1,34 +1,40 @@
 import Link from 'next/link';
 import React from 'react';
 import { ROUTE } from '@/constants';
+import Head from 'next/head';
 
 function PagesMain() {
   return (
-    <div id="page-main">
-      <h1>Hello, Next.js!</h1>
-      <ul>
-        <li>
-          <Link href={ROUTE.PAGE.INDEX}>Page Router Index</Link>
-        </li>
-        <li>
-          <Link href={ROUTE.PAGE.TODO}>Todo</Link>
-        </li>
-        <li>
-          <Link href={ROUTE.PAGE.TABS}>Tabs</Link>
-        </li>
-      </ul>
-      <hr />
-      <ul>
-        <h2>App Router</h2>
-        <li>
-          <Link href={ROUTE.INDEX}>App Router Index</Link>
-        </li>
-        <li>
-          <Link href={ROUTE.TODO}>Todo</Link>
-        </li>
+    <>
+      <Head>
+        <title>Page Route Index</title>
+      </Head>
+      <div id="page-main">
+        <h1>Hello, Next.js!</h1>
+        <ul>
+          <li>
+            <Link href={ROUTE.PAGE.INDEX}>Page Router Index</Link>
+          </li>
+          <li>
+            <Link href={ROUTE.PAGE.TODO}>Todo</Link>
+          </li>
+          <li>
+            <Link href={ROUTE.PAGE.TABS}>Tabs</Link>
+          </li>
+        </ul>
+        <hr />
+        <ul>
+          <h2>App Router</h2>
+          <li>
+            <Link href={ROUTE.INDEX}>App Router Index</Link>
+          </li>
+          <li>
+            <Link href={ROUTE.TODO}>Todo</Link>
+          </li>
 
-      </ul>
-    </div>
+        </ul>
+      </div>
+    </>
   );
 }
 
